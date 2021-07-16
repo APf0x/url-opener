138c90fa8a50fe9db23521a1883099c72a1ff068
APf0x <77631767+APf0x@users.noreply.github.com>
2021-07-16 19:19:31 +0200

    Update url opener.py

--- url opener.py
+++ url opener.py
@@ -6,22 +6,3 @@ for url in f.readlines():
     urls = url.strip()
     print(urls)
     subprocess.Popen(["C:\Program Files (x86)\Google\Chrome\Application\chrome.exe", "-incognito", urls])
-
-#subprocess.Popen(["C:\Program Files (x86)\Google\Chrome\Application\chrome.exe", "-incognito", urls]])
-
-
-
-
-'''
-import subprocess
-import time
-
-f = open("urls.txt")
-urls = [url.strip() for url in f.readlines()]
-
-print(urls)
-
-
-
-# subprocess.Popen(["C:\Program Files (x86)\Google\Chrome\Application\chrome.exe", "-incognito", urls]])
-'''
